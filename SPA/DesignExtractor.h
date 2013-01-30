e21ab20954441c815aed088ad589898a908db28c
qinchuan <a0087381@nus.edu.sg>
2013-01-30 15:31:39 +0800

    copied such that methods to de

--- SPA/DesignExtractor.h
+++ SPA/DesignExtractor.h
@@ -14,7 +14,40 @@ public:
 	STATEMENT_LIST * getAllWhiles();
 	STATEMENT_LIST * getAllIfs();
 
+	//from such that 
+	FOLLOWS_LIST getFollowsResult(TYPE type, STATEMENT_NUM s1, STATEMENT_NUM s2);
+	 FOLLOWS_LIST getFollowsStarResult(TYPE type, STATEMENT_NUM s1, STATEMENT_NUM s2);
+	 FOLLOWS_LIST getFollowsResult(TYPE type1, TYPE type2);
+	 FOLLOWS_LIST getFollowsStarResult(TYPE type1, TYPE type2);
+	 bool getIsFollowsResult(STATEMENT_NUM s1, STATEMENT_NUM s2);
+	 bool getIsFollowsStarResult(STATEMENT_NUM s1, STATEMENT_NUM s2);
+
+	 PARENT_LIST getParentResult(TYPE type1, TYPE type2);
+	 PARENT_LIST getParentStarResult(TYPE type1, TYPE type2);
+	 PARENT_LIST getParentResult(TYPE type, STATEMENT_NUM s1, STATEMENT_NUM s2);
+	 PARENT_LIST getParentStarResult(TYPE type, STATEMENT_NUM s1, STATEMENT_NUM s2);
+	 bool getIsParentResult(STATEMENT_NUM s1, STATEMENT_NUM s2);
+	 bool getIsParentStarResult(STATEMENT_NUM s1, STATEMENT_NUM s2);
+
+	 MODIFIES_LIST getModifiesResult(TYPE type, int arg1, VAR_INDEX v1);
+	 bool getIsModifiesResult(TYPE type, int arg1, VAR_INDEX v1);
+
+	 USES_LIST getUsesResult(TYPE type, int arg1, VAR_INDEX v1);
+	 bool getIsUsesResult(TYPE type, int arg1, VAR_INDEX v1);
+
+
+
 private:
 	PKB* pkb;
+	 void computeTypeOnly(list<pair<int, int>> &result, list<pair<int, int>> &listLst, TYPE type1, TYPE type2);
+	 void computeParent(PARENT_LIST &result, PARENT_LIST tmp, TYPE type, STATEMENT_NUM s1, STATEMENT_NUM s2);
+	 void computeFollows(FOLLOWS_LIST &result, FOLLOWS_LIST tmp, TYPE type, STATEMENT_NUM s1, STATEMENT_NUM s2);
+	 PARENT_LIST getParentStar(STATEMENT_NUM stmt1, STATEMENT_NUM stmt2);
+	 void computeParentStar(STATEMENT_NUM stmt1, STATEMENT_NUM stmt2, list<int> &result);
+	 FOLLOWS_LIST getFollowsStar(STATEMENT_NUM stmt1, STATEMENT_NUM stmt2);
+	 void computeFollowsStar(STATEMENT_NUM stmt1, STATEMENT_NUM stmt2, list<int> &result);
+	
+	
+	 void iterateAndStore(list<pair<int, int>> &result, list<pair<int, int>> tmp, int v1);
 };
 
