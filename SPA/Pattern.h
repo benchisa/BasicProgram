ac375bb0ee1e8512f3feec7b3f92cbfb9a04ce87
Benjamin <benchisa@gmail.com>
2013-02-03 17:54:51 +0800

    Completed Pattern class.

--- SPA/Pattern.h
+++ SPA/Pattern.h
@@ -3,27 +3,22 @@
 #include "PKB.h"
 #include "AST.h"
 #include "QTREE.h"
+#include "PKB.h"
 
 class Pattern
 {
 
 public:
 	
-	Pattern(void);
+	Pattern(PKB* pkb);
 	~Pattern(void);
-	
-	void setPKB(PKB* p);
-	//type can be while or assign
-	bool isPattern(hash_map<int, TYPE>* qvartable, QTREE* expression);
-	hash_map<int, TYPE>* getPatternStmt(hash_map<int, TYPE>*qVarTable,QTREE* expression);
+	//static PKB *pkb;
+	//void setPKB(PKB* p);
+	RELATION_LIST* evaluatePattern(QUERYTABLE * qtable);
 private:
-	STATEMENT_LIST* getPatternAssign(VAR_INDEX index,QTREE* Expression);
-	STATEMENT_LIST* getPatternWhile(VAR_INDEX index);
-	STATEMENT_LIST* getPatternIf(VAR_INDEX index);
-	bool isPatternAssign(VAR_INDEX index,QTREE* expression);
-	bool isPatternWhile(VAR_INDEX index);
-	bool isSubTree(AST* rootTree, QTREE* subTree);
-
-	PKB *pkb;
+	PKB *p;
+	AST * ast;
+	VAR_LIST *varList;
+	RELATION_LIST* list;
 };
 
