6f7179a5be85be1abc7ede5ddddbb74b0e977294
Lim Seok Min <lseokmin@gmail.com>
2013-02-07 11:30:04 +0800

    updated test cases

--- UnitTesting/CFGTest.cpp
+++ UnitTesting/CFGTest.cpp
@@ -1,25 +1,32 @@
 #include "CFGTest.h"
 #include "../SPA/CFG.h"
 
-//CPPUNIT_TEST_SUITE_REGISTRATION( CFGTest );
+CPPUNIT_TEST_SUITE_REGISTRATION( CFGTest );
+
+void CFGTest::setUp(){}
+void CFGTest::tearDown(){}
 
 void CFGTest::testCFG(){
-	CFG* cfg = new CFG(5);
-	cfg->addEdge(1,2);
-	cfg->addEdge(2,3);
-	cfg->addEdge(3,4);
-	cfg->addEdge(4,5);
-	cfg->addEdge(5,1);
+	CFG *cfg2 = new CFG(6);
+	cfg2->addEdge(1,2);
+	cfg2->addEdge(2,3);
+	cfg2->addEdge(2,4);
+	cfg2->addEdge(3,5);
+	cfg2->addEdge(4,5);
+	cfg2->addEdge(5,1);
 
+	// this is only the progline
+	CPPUNIT_ASSERT(cfg2->getAllProgLines(1,0).size() == 5);
+	CPPUNIT_ASSERT(cfg2->getAllProgLines(0,6).size() == 0);
+	/*list<PROG_LINE> result = cfg2->getAllProgLines(2,3);
+	cout << "Result: " << result.size() << "\n";
+	list<PROG_LINE>::iterator itr = result.begin();
+	while(itr!=result.end()){
+		cout << "> " << *itr << "\n";
+		itr++;
+	}*/
+	CPPUNIT_ASSERT(cfg2->getAllProgLines(2,3).size() == 1);
 
-	CPPUNIT_ASSERT(cfg->isNext(1,2));
-	CPPUNIT_ASSERT(!cfg->isNext(3,2));
-	CPPUNIT_ASSERT(!cfg->isNext(10,2));
-	CPPUNIT_ASSERT(cfg->findAllPaths(1,0).size() == 5);
-	CPPUNIT_ASSERT(cfg->findAllPaths(0,0).size() == 5);
-	CPPUNIT_ASSERT(cfg->findAllPaths(0,5).size() == 5);
-	CPPUNIT_ASSERT(cfg->findAllPaths(1,5).size() == 5);
-	CPPUNIT_ASSERT(cfg->findAllPaths(2,5).size() == 5);
-	CPPUNIT_ASSERT(cfg->findAllPaths(3,5).size() == 5);
-	CPPUNIT_ASSERT(cfg->findAllPaths(5,5).size() == 5);
+	// this is in Pairs
+	CPPUNIT_ASSERT(cfg2->getAllPaths(1,0).size() == 6);
 }
