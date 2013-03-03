abdbdb583faef00be6eded721365e74a6817eecb
Zhang Xi <a0091390@nus.edu.sg>
2013-03-03 15:28:15 +0800

    dd

--- SPA/SuchThatClause.cpp
+++ SPA/SuchThatClause.cpp
@@ -75,13 +75,24 @@ RELATION_LIST* SuchThatClause::evaluateSuchThat(){
 				}
 			}
 			if(relType==MODIFIES){
-
-				if(extractor->getIsModifiesResult(firstRel->getType(),firstRel->getData(),secondRel->getData())){
+				TYPE modType;
+				if(firstRel->getType()==PROCEDURE){
+					modType = PROCEDURE;
+				}else{
+					modType = STATEMENT;
+				}
+				if(extractor->getIsModifiesResult(modType,firstRel->getData(),secondRel->getData())){
 					relList->push_back(pair<int,int>(firstRel->getData(),secondRel->getData()));
 				}
 			}
 			if(relType==USES){
-				if(extractor->getIsUsesResult(firstRel->getType(),firstRel->getData(),secondRel->getData())){
+				TYPE useType;
+				if(firstRel->getType()==PROCEDURE){
+					useType = PROCEDURE;
+				}else{
+					useType = STATEMENT;
+				}
+				if(extractor->getIsUsesResult(useType,firstRel->getData(),secondRel->getData())){
 					relList->push_back(pair<int,int>(firstRel->getData(),secondRel->getData()));
 				}
 			}
