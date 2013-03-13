93dde865f4c7f52f1954328eb2d02b4c011f8554
qinchuan <a0087381@nus.edu.sg>
2013-03-13 21:40:46 +0800

    Update EvaluateParents.h

--- SPA/EvaluateParents.h
+++ SPA/EvaluateParents.h
@@ -19,6 +19,6 @@ public:
 private:
 	static void EvaluateParents::computeParent(PARENT_LIST &result,PARENT_LIST tmpLst, TYPE type, STATEMENT_NUM s1, STATEMENT_NUM s2);
 	static FOLLOWS_LIST EvaluateParents::getParentStar(STATEMENT_NUM stmt1, STATEMENT_NUM stmt2);
-	static void EvaluateParents::computeParentStar(STATEMENT_NUM stmt1, STATEMENT_NUM stmt2, list<int> &result);
+	static list<int> EvaluateParents::computeParentStar(STATEMENT_NUM stmt1, STATEMENT_NUM stmt2);
 };
 
