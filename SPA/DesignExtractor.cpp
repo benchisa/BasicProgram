b3120d3af8537280dea32e627099a542305ebc54
qinchuan <a0087381@nus.edu.sg>
2013-03-13 21:18:30 +0800

    Update DesignExtractor.cpp

--- SPA/DesignExtractor.cpp
+++ SPA/DesignExtractor.cpp
@@ -159,3 +159,8 @@ bool DesignExtractor::getIsAffectResult(STATEMENT_NUM stmt1, STATEMENT_NUM stmt2
 AFFECT_LIST DesignExtractor::getAffectResult(STATEMENT_NUM stmt1, STATEMENT_NUM stmt2){
 	return Affects::getAffectResult(stmt1, stmt2);
 }
+
+bool	DesignExtractpr::getIsAffectResult(STATEMENT_NUM stmt1, STATEMENT_NUM stmt2){
+	
+	return Affects::getIsAffectStarResult(stmt1,stmt2)
+}
