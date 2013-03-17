671346d83a840ab8833eac6defe926af43b21b70
qinchuan <a0087381@nus.edu.sg>
2013-03-17 17:34:30 +0800

    Update Affects.h

--- SPA/Affects.h
+++ SPA/Affects.h
@@ -12,11 +12,13 @@ public:
 	static PKB *		pkb;
 
 	static bool			getIsAffectResult(STATEMENT_NUM stmt1, STATEMENT_NUM stmt2);
-	static AFFECT_LIST	getAffectResult(STATEMENT_NUM stmt1, STATEMENT_NUM stmt2);
-	static	bool getIsAffectStarResult(STATEMENT_NUM stmt1, STATEMENT_NUM stmt2);
+	static AFFECT_LIST		getAffectResult(STATEMENT_NUM stmt1, STATEMENT_NUM stmt2);
+	static	bool 			getIsAffectStarResult(STATEMENT_NUM stmt1, STATEMENT_NUM stmt2);
 
 private:
 	
 	static bool			computeIsAffect(int starting, int ending, int varIndex);
+	static bool         		computeIsAffectStar(int starting, int ending); 
+
 
 };
