88c725a190ce18ad4e405464ea0f5f621dcc8905
qinchuan <a0087381@nus.edu.sg>
2013-04-13 23:48:45 +0800

    Update EvaluateNext.h

--- SPA/EvaluateNext.h
+++ SPA/EvaluateNext.h
@@ -17,9 +17,10 @@ public:
 	static bool			isNextResult(PROG_LINE, PROG_LINE);
 	static NEXT_LIST	getNextStarResult(PROG_LINE, PROG_LINE);
 	static NEXT_LIST	getNextResult(PROG_LINE, PROG_LINE);
+	static NEXT_LIST    getAllPaths(PROG_LINE, PROG_LINE);
 
 private:
 	static vector<PROG_LINE> cfgWhileKeepers;
 	static AST * buildCFG(AST *);
+	 
 };
-
